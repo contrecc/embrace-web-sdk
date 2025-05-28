eed3eed2281059b345c3a371a544d9c5d483d192
joaquin-diaz <32420835+joaquin-diaz@users.noreply.github.com>
2025-05-28 13:50:41 -0300

    chore: bundle size small improvements (#399)

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,90 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import { defineConfig } from 'rollup';
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+import pkg from './package.json' with { type: 'json' };
 
-export default {
-  input: 'build/esm/index.js',
-  plugins: [
-    commonjs(),
-    resolve({
-      browser: true,
-    }),
-  ],
-  output: {
-    file: 'build/esm/bundle.js',
-    format: 'iife',
-    name: 'EmbraceWebSdk',
+// Treat all deps as external for NPM build
+const externalDeps = [
+  'tslib',
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+];
+
+export default defineConfig([
+  // ESM Build
+  {
+    input: 'src/index.ts',
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.esm.json',
+      }),
+      terser(),
+    ],
+    output: {
+      dir: 'build/esm',
+      format: 'esm',
+      sourcemap: true,
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+    },
+    external: externalDeps,
   },
-};
+
+  // ESNext build
+  {
+    input: 'src/index.ts',
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.esnext.json',
+      }),
+      terser(),
+    ],
+    output: {
+      dir: 'build/esnext',
+      format: 'esm',
+      sourcemap: true,
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+    },
+    external: externalDeps,
+  },
+
+  // CJS build
+  {
+    input: 'src/index.ts',
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json',
+      }),
+      terser(),
+    ],
+    output: {
+      dir: 'build/src',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    external: externalDeps,
+  },
+
+  // CDN Build
+  {
+    input: 'src/index.ts',
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.esm.json',
+      }),
+      commonjs(),
+      resolve({
+        browser: true,
+      }),
+      terser(),
+    ],
+    output: {
+      file: 'build/iife/bundle.js',
+      format: 'iife',
+      name: 'EmbraceWebSdk',
+    },
+  },
+]);
